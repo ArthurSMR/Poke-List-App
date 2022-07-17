71587b7358d9913d06a3510dd83bf90aaeb99b92
ArthurSMR <arthursmr17@gmail.com>
2022-07-17 00:13:25 -0300

    Cell Setup

--- PokeList/PokeList/Home/HomeView.swift
+++ PokeList/PokeList/Home/HomeView.swift
@@ -20,6 +20,20 @@ final class HomeView: UIView {
     // MARK: Properties
     
     weak var delegate: HomeViewDelegate?
+    private var pokemons: [PokemonModel] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(PokemonListCell.self,
+                           forCellReuseIdentifier: PokemonListCell.identifier)
+        return tableView
+    }()
     
     private lazy var label: UILabel = {
        let label = UILabel()
@@ -44,7 +58,31 @@ final class HomeView: UIView {
 extension HomeView: HomeViewProtocol {
     
     func updateView(withPokemons pokemons: [PokemonModel]) {
+        self.pokemons = pokemons
+    }
+}
+
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        if let cell = tableView.dequeueReusableCell(withIdentifier: PokemonListCell.identifier, for: indexPath) as? PokemonListCell {
+
+            let pokemon = pokemons[indexPath.row]
+
+            cell.setup(pokemonName: pokemon.name)
+
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
 
@@ -52,22 +90,22 @@ extension HomeView: HomeViewProtocol {
 extension HomeView: ViewCodable {
     
     func setupHierarchy() {
-        addSubview(label)
+        addSubview(tableView)
     }
     
     func setupConstraints() {
         
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor),
-            label.heightAnchor.constraint(equalToConstant: 40)
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
         ])
     }
     
     func render() {
-        backgroundColor = .white
-        
-        label.backgroundColor = .red
-        label.textColor = .white
+        backgroundColor = .blue
     }
 }
---
+++ PokeList/PokeList/Home/PokemonListCell.swift
@@ -0,0 +1,83 @@
+//
+//  PokemonListCell.swift
+//  PokeList
+//
+//  Created by Arthur Rodrigues on 16/07/22.
+//
+
+import UIKit
+
+final class PokemonListCell: UITableViewCell {
+    
+    static let identifier: String = String(describing: PokemonListCell.self)
+    
+    private let pokemonImageView: UIImageView = {
+       let img = UIImageView()
+        img.contentMode = .scaleToFill
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.layer.cornerRadius = 20
+        img.clipsToBounds = true
+        img.image = UIImage(named: "placeholder")
+        return img
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .orange
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        pokemonImageView.layer.masksToBounds = true
+        pokemonImageView.layer.cornerRadius = pokemonImageView.frame.size.height / 2
+    }
+    
+    func setup(pokemonName: String, imageURL: URL? = nil) {
+        label.text = pokemonName
+    }
+    
+    private func setupView() {
+        
+        stackView.addArrangedSubview(pokemonImageView)
+        stackView.addArrangedSubview(label)
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            pokemonImageView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.8),
+            pokemonImageView.heightAnchor.constraint(equalTo: pokemonImageView.widthAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 16)
+        ])
+        
+        label.textColor = .white
+    }
+}
